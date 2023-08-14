07d1c46fe1c62add6fc4bc9afec0a076297b4073
Yvonne <a0905001923@gmail.com>
2023-08-14 13:44:59 +0800

    main

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,24 +1,14 @@
 "use client";
 
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #bf4f74;
-`;
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
-
 const page = () => {
   return (
-    <div>
-      <Wrapper>
-        <Title>Hello World!</Title>
-      </Wrapper>
-      <div className="text-3xl font-bold underline">page</div>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
     </div>
   );
 };
