f9160c26db345e0e49e3d048edf3cdd12bb88fd2
OAO1430 <trinky.cw.su@gmail.com>
2023-08-16 10:07:37 +0800

    Add: OpenAI test

---
+++ back_end/server/models/openai_test.js
@@ -0,0 +1,41 @@
+// const { Configuration, OpenAIApi } = require("openai");
+// require('dotenv').config({ path: __dirname + `/../../.env` });
+
+// console.log(process.env.OPENAI_API_KEY)
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// const completion = openai.createChatCompletion({
+//   model: "gpt-3.5-turbo",
+//   messages: [{"role": "system", "content": "You are a helpful assistant."}, {role: "user", content: "Hello world"}],
+// }).then().catch(error => {console.error("Error:", error);
+// });
+
+// console.log(completion)
+// console.log(completion.data.choices[0].message);
+
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config({ path: __dirname + `/../../.env` });
+
+async function main() {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {"role": "system", "content": "You are a helpful assistant."},
+      {role: "user", content: "Hello world"}
+    ],
+  });
+
+  console.log(completion.data.choices[0].message);
+}
+
+main().catch(error => {
+  console.error("Error:", error);
+});
