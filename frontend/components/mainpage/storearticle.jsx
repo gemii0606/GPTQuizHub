3bfdeed201d2c0a9e94d5eac6dd040190b24f576
Yvonne <a0905001923@gmail.com>
2023-08-16 17:30:46 +0800

    docs(1): 1
    N

--- frontend/components/mainpage/storearticle.jsx
+++ frontend/components/mainpage/storearticle.jsx
@@ -2,46 +2,40 @@ import React, { useState } from "react";
 import Swal from "sweetalert2";
 
 function Storearticle() {
-  const [total, setTotal] = useState(""); // 初始總題數
-  const [easy, setEasy] = useState(""); // 初始簡易題數
-  const [medium, setMedium] = useState(""); // 初始中等題數
+  const [easy, setEasy] = useState("");
+  const [medium, setMedium] = useState("");
   const [hard, setHard] = useState("");
-  function error1() {
-    Swal.fire(
-      "錯誤:題目大於10題",
-      "最多產生10題喔！",
-      "error",
-    );
-  }
-  function error2(totalQuestion) {
-    Swal.fire(
-      "錯誤：簡易、中等和困難題目總數大於總題數",
-      `你設定了${totalQuestion}題`,
-      "error",
-    );
-  }
-  function error3(totalQuestion) {
-    Swal.fire(
-      "錯誤：簡易、中等和困難題目總數少於總題數",
-      `你設定了${totalQuestion}題`,
-      "error",
-    );
+  const [errorMessage, setErrorMessage] = useState(""); // State to hold error message
+
+  function handleInputChange(e, difficulty) {
+    const newValue = e.target.value;
+    switch (difficulty) {
+      case "easy":
+        setEasy(newValue);
+        break;
+      case "medium":
+        setMedium(newValue);
+        break;
+      case "hard":
+        setHard(newValue);
+        break;
+      default:
+        break;
+    }
+    // eslint-disable-next-line max-len
+    const totalQuestion = parseInt(easy || 0, 10) + parseInt(medium || 0, 10) + parseInt(hard || 0, 10);
+    if (totalQuestion > 10) {
+      setErrorMessage("題數不可超過10題");
+    } else {
+      setErrorMessage("");
+    }
   }
   function handleSubmit() {
-    const totalQuestion = parseInt(easy, 10) + parseInt(medium, 10) + parseInt(hard, 10);
-    if (total > 10) {
-      error1();
-      return;
-    }
-    if (totalQuestion > total) {
-      error2(totalQuestion);
-      return;
-    }
-    if (totalQuestion < total) {
-      error3(totalQuestion);
-      return;
-    }
-    console.log("submit!");
+    Swal.fire(
+      "Succesfully submit!",
+      "Let create the questions~",
+      "error",
+    );
   }
   return (
     <div className="p-8 m-0">
@@ -59,36 +53,42 @@ function Storearticle() {
         <textarea required className="w-full px-3 py-2 mb-2 rounded-md bg-slate-200 h-[50vh] ring-1 ring-[#8198BF] hover:ring-2 hover:ring-[#8198BF] resize-none" placeholder="Relation between Java and Javascript is like dog and hotdog." />
         <p className="mt-1 mb-2 text-base">題目設定</p>
         <div className="flex items-center w-full h-24 p-2 bg-white rounded">
-          <p className="ml-6 text-base font-bold">總題數:</p>
-          <input
-            required
-            placeholder="最多10題"
-            className="px-3 py-2 ml-2 rounded-md w-24 bg-slate-200 ring-1 ring-[#8198BF] hover:ring-2 hover:ring-[#8198BF]"
-            value={total}
-            onChange={(e) => setTotal(e.target.value)}
-          />
-          <p className="ml-10 text-base font-bold">題目難易度</p>
+          <p className="justify-center mt-4 ml-10 text-base font-bold">
+            設定題目難易度
+            <br />
+            (最多共十題問題)
+          </p>
           <p className="ml-6 text-base font-bold">簡易:</p>
           <input
             required
+            type="number"
+            name="easy"
+            min="0"
+            max="10"
+            onChange={(e) => handleInputChange(e, "easy")}
             className="px-3 py-2 ml-2 rounded-md w-16 bg-slate-200 ring-1 ring-[#8198BF] hover:ring-2 hover:ring-[#8198BF]"
-            value={easy}
-            onChange={(e) => setEasy(e.target.value)}
           />
           <p className="ml-6 text-base font-bold">中等:</p>
           <input
             required
+            type="number"
+            name="middle"
+            min="0"
+            max="10"
+            onChange={(e) => handleInputChange(e, "medium")}
             className="px-3 py-2 ml-2 rounded-md w-16 bg-slate-200 ring-1 ring-[#8198BF] hover:ring-2 hover:ring-[#8198BF]"
-            value={medium}
-            onChange={(e) => setMedium(e.target.value)}
           />
           <p className="ml-6 text-base font-bold">困難:</p>
           <input
             required
+            type="number"
+            name="hard"
+            min="0"
+            max="10"
+            onChange={(e) => handleInputChange(e, "hard")}
             className="px-3 py-2 ml-2 rounded-md w-16 bg-slate-200 ring-1 ring-[#8198BF] hover:ring-2 hover:ring-[#8198BF]"
-            value={hard}
-            onChange={(e) => setHard(e.target.value)}
           />
+          {errorMessage && <p className="ml-2 text-red-500">{errorMessage}</p>}
         </div>
         <button type="submit" className="block bg-[#8198BF] text-white px-6 py-1 rounded-md mt-4 ml-auto hover:bg-[#638ace]">生成題目</button>
       </form>
