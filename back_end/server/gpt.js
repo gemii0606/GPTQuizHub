e111041a1909d8aad26e588fad7876390e0e3abf
gemii0606 <gemii0606@gmail.com>
2023-08-16 10:58:21 +0800

    Update gpt.js

--- back_end/server/gpt.js
+++ back_end/server/gpt.js
@@ -6,12 +6,39 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const content = 
+`
+Docker 哲學
+Docker 的基本哲學 Build and Ship any Application Anywhere，也就是讓你可以建置任何應用程式並運行在任何地方。它讓我們可以分離應用程式運行的基礎設施(infrastructure)，快速的建立、發佈應用程式。開發應用程式時也可以專注開發應用，而不用花太多的時間在安裝/運行環境的設置。
+
+Docker是什麼？使用Docker的好處與實戰實例
+
+Docker 與虛擬主機不同
+Docker 運行應用程式的環境被稱作 容器 (container)。它不像虛擬機器(virtual machine )一樣需要 虛擬機器監視器(hypervisor)模擬出軟體、韌體或硬體，反而直接運行在宿主機（host machine）的核心。以執行應用程式的運行來說，虛擬機器需要模擬出整套作業系統才能運行應用程式，而應用程式容器則是直接運行在宿主機上。不僅如此，宿主機也可以是虛擬機器。因此，它比虛擬機器又更為輕量級、執行啟動又更快。
+
+Docker 可以做什麼？
+一致性的發佈環境
+讓開發和發佈有統一的標準環境，這有利於持續性整合與發佈continuous integration and continuous delivery (CI/CD) 工作流程。開發者撰寫程式碼，並運行在開發者的電腦的容器中，而其它部門也使用容器運行他們的應用程式。當所有的應用程式協同運作、測試都沒問題後，就可以發佈在同樣讓容器運行的正式環境中。若在測試中有任何容器內的程式有錯誤，只要修正部分程式碼，又可以快速的重新整合後發佈。
+可攜性發佈和按需式縮放(scaling)
+容器可以容易地運行在不同的環境，像是開發者電腦、虛擬機器、雲端環境或是雲端和本地的混合環境。因為容器的可攜性和輕量級地運行，我們可以輕易的按照商業需求擴張和縮小應用程式的實體數量。
+在同一台機器運行更多的工作
+容器提供輕量級的沙盒環境，在同一台機器可以運行多個容器共享硬體資源，不像虛擬機器需要模擬整個主機，它所需要的資源更多。
+基礎設施即代碼(Infrastructure as Code)及軟體定義網路
+Docker 的架構中提供 REST API 和客戶端 Docker CLI 來控制 Docker 物件，像是：images、containers、networks和 volumes。因此，各個容器的協同運作、網路、檔案系統都是可以被軟體定義、控制，像是用 Docker Compose File 和 Swarm。舉個實務上常常看到的例子：
+代碼修改網頁伺服器應用程式所監聽的 port 和掛載它所需的資料。
+代碼控制應用程式副本的縮放。
+因為代碼控制基礎設施，所以代碼可以保存基礎設施的設定也可以進行版本控制。
+===============================================================
+幫我針對這篇文章的內容出一份測驗，總共5題選擇題，一題4個選項。2題困難，1題簡單，2題普通，每一題要標註題目難易度和題型和正確答案和答案解釋。用繁體中文回答。不要其他多餘的回答，幫我轉成JSON的格式顯示
+json格式包含id,created_at而每一個question和option都有獨特的id
+`
+
 async function main () {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     max_tokens: 128,
-    messages: [{"role": "system", "content": "You are a helpful assistant."}, {role: "user", content: "Hello world"}],
+    messages: [{"role": "system", "content": "你現在是一個厲害的出題老師"}, {role: "user", content: content}],
   });
-  console.log(completion);
+  console.log(completion.data.choices[0].message);
 }
 main();
