7e07a10b28a28b4471d23d03d1c10e641d65fe24
Jarenchi <jaren719@gmail.com>
2023-08-17 17:29:03 +0800

    feat(questionbankcard.jsx): add questionbankcard

--- frontend/app/questionsbanks/page.jsx
+++ frontend/app/questionsbanks/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import QuestionBankSideBar from "../../components/QuestionBankSideBar";
+import QuestionsBanksCard from "../../components/QuestionBankCard";
 
 const MockData = [
   {
@@ -19,32 +19,7 @@ const MockData = [
   },
 ];
 const questionsBankItems = MockData.map((questionsBank) => (
-  <div
-    key={questionsBank.id}
-    className="flex items-center justify-between px-4 py-2 mb-3 border rounded-lg"
-  >
-    <div>
-      <p className="max-w-6xl mr-3 text-2xl font-bold leading-6 truncate">{questionsBank.title}</p>
-    </div>
-    <div className="flex items-center">
-      <div className="block text-base font-bold text-white bg-[#8198BF] py-2.5 px-4 rounded-md hover:opacity-50 mr-5">
-        <Link
-          href={`/questionsbanks/${questionsBank.id}`}
-          className="flex items-center cursor-pointer"
-        >
-          複習
-        </Link>
-      </div>
-      <div>
-        <Link
-          href={`/quiz/${questionsBank.id}`}
-          className="block text-base font-bold text-white bg-[#25B857] py-2.5 px-4 rounded-md hover:opacity-50"
-        >
-          測驗連結
-        </Link>
-      </div>
-    </div>
-  </div>
+  <QuestionsBanksCard questionsBank={questionsBank} key={questionsBank.id} />
 ));
 function Page() {
   return (
