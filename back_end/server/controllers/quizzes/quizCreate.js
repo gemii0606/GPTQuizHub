8d6333d4a090242f8d8bf9b80871f828898fd64f
gemii0606 <gemii0606@gmail.com>
2023-08-17 23:00:34 +0800

    1

--- back_end/server/controllers/quizzes/quizCreate.js
+++ back_end/server/controllers/quizzes/quizCreate.js
@@ -35,7 +35,9 @@ const quizCreate = async (req, res) => {
             total,
             insertQuiz
         }
-        console.log(insertQuiz)
+        
+        const ans = await quizzesCollection.findOne({_id:insertQuiz.insertedId});
+        console.log(ans)
         console.log(data)
         // axios.post('https://13.210.26.62/api/1.0/gptquizgenerator', data, { timeout: 120000 });
 
