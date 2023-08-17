0fdf9aa88afcb2cc36fc47c797bc64f0d112a8ca
gemii0606 <gemii0606@gmail.com>
2023-08-17 11:34:59 +0800

    Update quizCreate.js

--- back_end/server/controllers/quizzes/quizCreate.js
+++ back_end/server/controllers/quizzes/quizCreate.js
@@ -65,7 +65,7 @@ const quizCreate = async (req, res) => {
                 role: "user", 
                 content: requestJson(
                     `
-                    ${article}
+                    ${article.content}
                     :::
                     幫我針對這篇文章的內容出一份測驗，總共${total}題選擇題，一題4個選項。${article.hard}題困難，${article.easy}題簡單，${article.normal}題普通，每一題要標註題目難易度和題型和正確答案和答案解釋。回答語言請與輸入的語言相同。不要其他多餘的回答。
                     請用JSON的格式回覆我，並且有id、difficulty、type、question、options、correct_answer、explanation
