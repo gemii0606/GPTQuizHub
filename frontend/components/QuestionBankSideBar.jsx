49d0334c8fb2d041710242680edfdfb36940d0d2
Jarenchi <jaren719@gmail.com>
2023-08-17 15:59:30 +0800

    feat(questionbanksidebar.jsx): add questionbanksidebar

---
+++ frontend/components/QuestionBankSideBar.jsx
@@ -0,0 +1,39 @@
+const mocktags = [
+  {
+    id: 1,
+    name: "tag1",
+  },
+  {
+    id: 2,
+    name: "tag2",
+  },
+  {
+    id: 3,
+    name: "tag3",
+  },
+  {
+    id: 4,
+    name: ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+  },
+];
+
+function QuestionBankSideBar() {
+  //   const [currentTag, setCurrentTag] = useState("未分類");
+  const tagsItems = mocktags.map((tag) => (
+    <p
+      key={tag.id}
+      type="button"
+      className="px-4 py-4 text-2xl font-bold border rounded-lg min-w-[15rem] max-w-[20rem] mt-4 hover:bg-[#8198BF] hover:text-white break-words"
+    >
+      {tag.name}
+    </p>
+  ));
+  return (
+    <div className="p-4 bg-white border rounded-xl">
+      <p className="text-2xl font-bold leading-6 break-words whitespace-pre-wrap">題庫分類</p>
+      {tagsItems}
+    </div>
+  );
+}
+
+export default QuestionBankSideBar;
