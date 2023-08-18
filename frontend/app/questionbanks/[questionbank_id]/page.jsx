2b45cab5985119aaa103cf81a697889ffe7cda0d
Jarenchi <jaren719@gmail.com>
2023-08-18 11:20:08 +0800

    feat(questionbanks page): add navbar

--- frontend/app/questionbanks/[questionbank_id]/page.jsx
+++ frontend/app/questionbanks/[questionbank_id]/page.jsx
@@ -1,9 +1,13 @@
+import Navbar from "../../../components/navbar";
 import QuestonBankCard from "../../../components/questionbanks/QuestionCard";
 
 function Page() {
   return (
-    <div className="flex justify-center mt-8">
-      <QuestonBankCard />
+    <div>
+      <Navbar />
+      <div className="flex justify-center mt-8">
+        <QuestonBankCard />
+      </div>
     </div>
   );
 }
