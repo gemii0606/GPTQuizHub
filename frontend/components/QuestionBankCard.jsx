7c8205c704041c6e78b579f609d357dc2a427401
Jarenchi <jaren719@gmail.com>
2023-08-18 18:41:44 +0800

    feat(questionbankcard): add delete button

--- frontend/components/QuestionBankCard.jsx
+++ frontend/components/QuestionBankCard.jsx
@@ -1,31 +1,41 @@
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Swal from "sweetalert2";
+import DeleteIcon from "../public/delete.png";
 
 function QuestionsBanksCard({ questionsBank }) {
   const [showQuizType, setShowQuizType] = useState(false);
+  function deleteQuestionBankHandler() {
+    Swal.fire({
+      title: "確定刪除?",
+      text: "你無法找回此題庫",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "確定刪除",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("成功", "題庫已刪除", "success");
+      }
+    });
+  }
   return (
-    <div
-      key={questionsBank.id}
-      className="flex items-center justify-between px-4 py-2 mb-3 border rounded-lg"
-    >
+    <div key={questionsBank.id} className="flex items-center justify-between px-4 py-2 mb-3 border rounded-lg">
       <div>
-        <p className="max-w-6xl mr-3 text-2xl font-bold leading-6 truncate">
-          {questionsBank.title}
-        </p>
+        <p className="max-w-6xl mr-3 text-2xl font-bold leading-6 truncate">{questionsBank.title}</p>
       </div>
       <div className="flex items-center">
         <div className="block text-base font-bold text-white bg-[#8198BF] py-2.5 px-4 rounded-md hover:opacity-50 mr-5">
-          <Link
-            href={`/questionsbanks/${questionsBank.id}`}
-            className="flex items-center cursor-pointer"
-          >
+          <Link href={`/questionsbanks/${questionsBank.id}`} className="flex items-center cursor-pointer">
             複習
           </Link>
         </div>
         <div>
           <button
             type="button"
-            className="relative text-base font-bold text-white bg-[#25B857] py-2.5 px-4 rounded-md hover:opacity-50"
+            className="relative text-base font-bold text-white bg-[#25B857] py-2.5 px-4 rounded-md hover:opacity-50 mr-5"
             onClick={() => setShowQuizType(!showQuizType)}
           >
             測驗形式
@@ -53,6 +63,9 @@ function QuestionsBanksCard({ questionsBank }) {
             </div>
           )}
         </div>
+        <button type="button" onClick={deleteQuestionBankHandler}>
+          <Image src={DeleteIcon} alt="delete-icon" width={30} height={30} />
+        </button>
       </div>
     </div>
   );
