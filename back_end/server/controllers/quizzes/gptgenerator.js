7dc21ab4337139d1dbeb2c76b392d7eb0a6fd877
gemii0606 <gemii0606@gmail.com>
2023-08-18 11:27:37 +0800

    1

--- back_end/server/controllers/quizzes/gptgenerator.js
+++ back_end/server/controllers/quizzes/gptgenerator.js
@@ -25,7 +25,7 @@ const template = {
        "option",
        "option",
        "option" ],
-     "correct_answer": 1,
+     "correct_answer": "1, 2, 3, 4",
      "explanation": "explanation"
      }]
  }
@@ -44,13 +44,13 @@ const gptquizgenerator = async (req, res) => {
             model: "gpt-3.5-turbo",
             // max_tokens: 128,
             messages: [
-                {role: "system", content: "你是位專業的出題老師"},
+                {role: "system", content: "你是位專業的出題老師，而且你是根據'''標記以上文章的主要語言來出同樣語言的題目"},
                 {
                 role: "user", 
                 content: requestJson(
                     `
                     ${article.content}
-                    :::
+                    '''
                     幫我針對這篇文章的內容出一份測驗，總共${total}題選擇題，一題4個選項。${article.hard}題困難，${article.easy}題簡單，${article.normal}題普通，每一題要標註題目難易度和題型和正確答案和答案解釋。回答語言請與輸入的語言相同。不要其他多餘的回答。
                     請用JSON的格式回覆我，並且有id、difficulty、type、question、options、correct_answer、explanation
                 `, template)
