3e7e3b67b9b2681bca6d3c1b1c90e9bfd38c4dd1
Jarenchi <jaren719@gmail.com>
2023-08-18 13:12:40 +0800

    feat(two-player page): try to do twoplayer feature

--- frontend/components/questionbanks/QuestionBankCard.jsx
+++ frontend/components/questionbanks/QuestionBankCard.jsx
@@ -4,21 +4,13 @@ import Link from "next/link";
 function QuestionsBanksCard({ questionsBank }) {
   const [showQuizType, setShowQuizType] = useState(false);
   return (
-    <div
-      key={questionsBank.id}
-      className="flex items-center justify-between px-4 py-2 mb-3 border rounded-lg"
-    >
+    <div key={questionsBank.id} className="flex items-center justify-between px-4 py-2 mb-3 border rounded-lg">
       <div>
-        <p className="max-w-6xl mr-3 text-2xl font-bold leading-6 truncate">
-          {questionsBank.title}
-        </p>
+        <p className="max-w-6xl mr-3 text-2xl font-bold leading-6 truncate">{questionsBank.title}</p>
       </div>
       <div className="flex items-center">
         <div className="block text-base font-bold text-white bg-[#8198BF] py-2.5 px-4 rounded-md hover:opacity-50 mr-5">
-          <Link
-            href={`/questionsbank/${questionsBank.id}`}
-            className="flex items-center cursor-pointer"
-          >
+          <Link href={`/questionbanks/${questionsBank.id}`} className="flex items-center cursor-pointer">
             複習
           </Link>
         </div>
