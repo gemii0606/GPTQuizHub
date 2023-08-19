27aa35b287628ebdb5d3e2ebbbd2a1623cff6040
OAO1430 <trinky.cw.su@gmail.com>
2023-08-19 15:24:18 +0800

    Add: quizHistoryList tags and cursor

--- back_end/server/controllers/quizzes/quizHistoryList.js
+++ back_end/server/controllers/quizzes/quizHistoryList.js
@@ -12,6 +12,8 @@ const quizHistoryList = async (req, res) => {
         const signInId = req.signInId;
         const db = client.db(dbName);
         const quizzesHistoryCollection = db.collection('quizzesHistory');
+        const cursor = req.query.cursor ? parseInt(atob(req.query.cursor)) : req.query.cursor;
+        const tag = req.query.tag ? parseInt(atob(req.query.tag)) : req.query.tag;
 
         const quizzes = await quizzesHistoryCollection.aggregate([
             {
@@ -27,7 +29,9 @@ const quizHistoryList = async (req, res) => {
             },
             {
               $match: {
-                "quiz.user_id": new ObjectId(signInId)
+                "quiz.user_id": new ObjectId(signInId),
+                created_at: cursor ? { $lt: cursor } : { $exists: true },
+                tag: tag ? tag : { $exists: true },
               }
             },
             {
