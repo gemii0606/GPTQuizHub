a40f7502cdf8d5de17dda33e04deedbe17948b7f
Jarenchi <jaren719@gmail.com>
2023-08-19 16:19:32 +0800

    refactor: refactor quizsetting and sharelink page

--- frontend/app/quiz/[quiz_id]/page.jsx
+++ frontend/app/quiz/[quiz_id]/page.jsx
@@ -33,8 +33,7 @@ const MockData = {
         },
       ],
       correct_answer: 1,
-      explanation:
-        "key用於協助React辨識元件的位置，從而確定元件的新增、移除或修改，以避免可能出現的錯誤。",
+      explanation: "key用於協助React辨識元件的位置，從而確定元件的新增、移除或修改，以避免可能出現的錯誤。",
     },
     {
       id: 2,
@@ -59,8 +58,7 @@ const MockData = {
         },
       ],
       correct_answer: 3,
-      explanation:
-        "正確的key需求是獨特唯一且穩定可預測的，不應使用index作為key，因為可能會導致渲染錯誤。",
+      explanation: "正確的key需求是獨特唯一且穩定可預測的，不應使用index作為key，因為可能會導致渲染錯誤。",
     },
     {
       id: 3,
@@ -85,14 +83,12 @@ const MockData = {
         },
       ],
       correct_answer: 3,
-      explanation:
-        "正確的key需求是獨特唯一且穩定可預測的，不應使用index作為key，因為可能會導致渲染錯誤。",
+      explanation: "正確的key需求是獨特唯一且穩定可預測的，不應使用index作為key，因為可能會導致渲染錯誤。",
     },
   ],
 };
 // TODO:隨機題目排序
 // TODO:根據題目數量選擇相對應的題數
-// TODO:把SWAL2加入THEN
 function Page() {
   const [quizStatus, setQuizStatus] = useState("start");
   const [questionIndex, setQuestionIndex] = useState(0);
@@ -157,50 +153,48 @@ function Page() {
     document.body.classList.toggle("modal-open");
   };
   const StartPage = (
-    <div className="flex justify-center mt-[10rem] ">
-      <div className="border border-black rounded-xl min-w-[60rem] min-h-[60rem] items-center">
-        <p className="text-center mt-[8rem] font-extrabold text-4xl">{MockData.title}</p>
-        <div className="flex flex-col items-center mt-24">
-          <button
-            type="button"
-            className="block px-24 py-4 text-4xl bg-[#8198BF] text-white rounded-xl mb-20"
-            onClick={ModalToggleHandler}
-          >
-            測驗設定
-          </button>
-          {showSetting && (
-            <QuizSetting
-              modalToggleHandler={ModalToggleHandler}
-              questionNumber={questionNumber}
-              setQuestionNumber={setQuestionNumber}
-              setQuestionSeconds={setQuestionSeconds}
-              questionSeconds={questionSeconds}
-              randomOptions={randomOptions}
-              setRandomOptions={setRandomOptions}
-              randomOrder={randomOrder}
-              setRandomOrder={setRandomOrder}
-            />
-          )}
-          <button
-            type="button"
-            onClick={() => {
-              setQuizStatus("process");
-              setSeconds(questionSeconds);
-            }}
-            className="block px-24 py-4 text-4xl bg-[#4783EA] text-white rounded-xl mb-20"
-          >
-            開始測驗
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              router.push("/questionbanks");
-            }}
-            className="block px-24 py-4 text-4xl bg-[#8198BF] text-white rounded-xl mb-20"
-          >
-            回到題庫
-          </button>
-        </div>
+    <div className="border border-black rounded-xl min-w-[60rem] min-h-[60rem] items-center">
+      <p className="text-center mt-[8rem] font-extrabold text-4xl">{MockData.title}</p>
+      <div className="flex flex-col items-center mt-24">
+        <button
+          type="button"
+          className="block px-24 py-4 text-4xl bg-[#8198BF] text-white rounded-xl mb-20"
+          onClick={ModalToggleHandler}
+        >
+          測驗設定
+        </button>
+        {showSetting && (
+          <QuizSetting
+            modalToggleHandler={ModalToggleHandler}
+            questionNumber={questionNumber}
+            setQuestionNumber={setQuestionNumber}
+            setQuestionSeconds={setQuestionSeconds}
+            questionSeconds={questionSeconds}
+            randomOptions={randomOptions}
+            setRandomOptions={setRandomOptions}
+            randomOrder={randomOrder}
+            setRandomOrder={setRandomOrder}
+          />
+        )}
+        <button
+          type="button"
+          onClick={() => {
+            setQuizStatus("process");
+            setSeconds(questionSeconds);
+          }}
+          className="block px-24 py-4 text-4xl bg-[#4783EA] text-white rounded-xl mb-20"
+        >
+          開始測驗
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            router.push("/questionbanks");
+          }}
+          className="block px-24 py-4 text-4xl bg-[#8198BF] text-white rounded-xl mb-20"
+        >
+          回到題庫
+        </button>
       </div>
     </div>
   );
@@ -240,76 +234,72 @@ function Page() {
     </button>
   ));
   const ProcessPage = (
-    <div className="flex justify-center mt-[10rem]">
-      <div className="border border-black rounded-xl min-w-[60rem] min-h-[60rem] items-center">
-        {MockData.questions.length > 0 && (
-          <div>
-            <div className="flex flex-col items-center mt-10">
-              <h1 className="mb-4 text-4xl">剩餘時間 : {seconds}</h1>
-              <p className="text-4xl">{MockData.questions[questionIndex].content}</p>
-              <div className="flex my-4">
-                <p className="mr-3 text-3xl">難度 :</p>
-                <p className="mr-6 text-3xl">{MockData.questions[questionIndex].difficulty}</p>
-                <p className="text-3xl">
-                  {questionIndex + 1} / {MockData.questions.length}
-                </p>
-              </div>
+    <div className="border border-black rounded-xl min-w-[60rem] min-h-[60rem] items-center">
+      {MockData.questions.length > 0 && (
+        <div>
+          <div className="flex flex-col items-center mt-10">
+            <h1 className="mb-4 text-4xl">剩餘時間 : {seconds}</h1>
+            <p className="text-4xl">{MockData.questions[questionIndex].content}</p>
+            <div className="flex my-4">
+              <p className="mr-3 text-3xl">難度 :</p>
+              <p className="mr-6 text-3xl">{MockData.questions[questionIndex].difficulty}</p>
+              <p className="text-3xl">
+                {questionIndex + 1} / {MockData.questions.length}
+              </p>
             </div>
-            <div className="flex flex-col items-center mb-10">{OptionsItems}</div>
           </div>
-        )}
-      </div>
+          <div className="flex flex-col items-center mb-10">{OptionsItems}</div>
+        </div>
+      )}
     </div>
   );
   const correctAnswers = MockData.questions.length - wrongAnswer.length;
   const accuracy = (correctAnswers / MockData.questions.length) * 100;
   const EndPage = (
-    <div className="flex justify-center mt-[10rem]">
-      <div className="flex flex-col border border-black rounded-xl min-w-[60rem] min-h-[60rem] items-center">
-        <div className="mt-[8rem]">
-          {accuracy === 100 && <p className="text-4xl">滿分，你超強</p>}
-          {accuracy > 60 && accuracy < 100 && <p className="text-4xl">還不錯</p>}
-          {accuracy < 60 && <p className="text-4xl">你超爛</p>}
-          <p className="text-4xl">答對率 : {accuracy.toFixed(2)}%</p>
-          <p className="text-4xl">總題數 : {MockData.questions.length}</p>
-          <p className="text-4xl">未答題數 : {unansweredQuestion.length}</p>
-          <p className="text-4xl">正確題數 : {correctAnswers}</p>
-          <p className="text-4xl">錯誤題數 : {wrongAnswer.length}</p>
-        </div>
-        <div>
-          <button
-            type="button"
-            onClick={() => {
-              setLoading(true);
-              setUnansweredQuestion([]);
-              setWrongAnswer([]);
-              setQuizStatus("start");
-              setSeconds(10);
-              setQuestionIndex(0);
-              setLoading(false);
-            }}
-            className="block px-24 py-4 text-4xl mr-8 bg-[#4783EA] text-white rounded-xl mt-20 disabled:opacity-50"
-          >
-            重新測驗
-          </button>
-          <button
-            type="button"
-            disabled={loading}
-            onClick={() => {
-              setLoading(true);
-              router.push("/questionbanks");
-              setLoading(false);
-            }}
-            className="block px-24 py-4 text-4xl bg-[#4783EA] text-white rounded-xl mt-20 disabled:opacity-50"
-          >
-            回到題庫
-          </button>
-        </div>
+    <div className="flex flex-col border border-black rounded-xl min-w-[60rem] min-h-[60rem] items-center">
+      <div className="mt-[8rem]">
+        {accuracy === 100 && <p className="text-4xl">滿分，你超強</p>}
+        {accuracy > 60 && accuracy < 100 && <p className="text-4xl">還不錯</p>}
+        {accuracy < 60 && <p className="text-4xl">你超爛</p>}
+        <p className="text-4xl">答對率 : {accuracy.toFixed(2)}%</p>
+        <p className="text-4xl">總題數 : {MockData.questions.length}</p>
+        <p className="text-4xl">未答題數 : {unansweredQuestion.length}</p>
+        <p className="text-4xl">正確題數 : {correctAnswers}</p>
+        <p className="text-4xl">錯誤題數 : {wrongAnswer.length}</p>
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setLoading(true);
+            setUnansweredQuestion([]);
+            setWrongAnswer([]);
+            setQuizStatus("start");
+            setSeconds(10);
+            setQuestionIndex(0);
+            setLoading(false);
+          }}
+          className="block px-24 py-4 text-4xl mr-8 bg-[#4783EA] text-white rounded-xl mt-20 disabled:opacity-50"
+        >
+          重新測驗
+        </button>
+        <button
+          type="button"
+          disabled={loading}
+          onClick={() => {
+            setLoading(true);
+            router.push("/questionbanks");
+            setLoading(false);
+          }}
+          className="block px-24 py-4 text-4xl bg-[#4783EA] text-white rounded-xl mt-20 disabled:opacity-50"
+        >
+          回到題庫
+        </button>
       </div>
     </div>
   );
   return (
-    <div>
+    <div className="flex justify-center mt-[5rem]">
       {quizStatus === "start" && StartPage}
       {quizStatus === "process" && ProcessPage}
       {quizStatus === "end" && EndPage}
--- frontend/components/QuizSetting.jsx
+++ frontend/components/QuizSetting.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import Swal from "sweetalert2";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 import Modal from "./layout/Modal";
+import Close from "../public/close.png";
 
 function QuizSetting({
   modalToggleHandler,
@@ -25,12 +27,8 @@ function QuizSetting({
     <Modal>
       <div className="flex flex-col items-center">
         <p className="pt-6 pb-4 text-3xl text-center text-black">測驗設定</p>
-        <button
-          type="button"
-          onClick={() => modalToggleHandler()}
-          className="absolute px-4 py-2 border border-black rounded-lg top-6 right-6"
-        >
-          關閉
+        <button type="button" onClick={() => modalToggleHandler()} className="absolute top-6 right-6">
+          <Image src={Close} alt="close-button" />
         </button>
         <div>
           <div className="flex items-center">
@@ -83,7 +81,12 @@ function QuizSetting({
                   <button
                     onClick={() => {
                       setCorrectRatio(correctRatioRef.current.value);
-                      Swal.fire("得分倍率修改成功", "", "success");
+                      Swal.fire({
+                        icon: "success",
+                        title: "得分倍率修改成功",
+                        showConfirmButton: false,
+                        timer: 1000,
+                      });
                     }}
                     type="button"
                     className="px-[2.125rem] py-4 text-white font-outfit font-normal text-3xl leading-6 rounded-md bg-sky-500"
@@ -107,7 +110,12 @@ function QuizSetting({
             <button
               onClick={() => {
                 setQuestionSeconds(questionSecondRef.current.value);
-                Swal.fire("修改成功", "", "success");
+                Swal.fire({
+                  icon: "success",
+                  title: "秒數修改成功",
+                  showConfirmButton: false,
+                  timer: 1000,
+                });
               }}
               type="button"
               className="px-[2.125rem] py-4 text-white font-outfit font-normal text-3xl leading-6 rounded-md bg-sky-500"
