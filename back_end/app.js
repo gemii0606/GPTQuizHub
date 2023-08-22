574321b8aad84b742f28b3f05adf5e84f405ac9b
gemii0606 <gemii0606@gmail.com>
2023-08-22 17:24:28 +0800

    Update app.js

--- back_end/app.js
+++ back_end/app.js
@@ -8,15 +8,15 @@ const io = socketIo(server); // 将 Socket.IO 集成到服务器
 
 // 其他 Express 中间件和路由设置
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected');
+//     });
     
-    // 在此处理其他 Socket.IO 事件
-});
+//     // 在此处理其他 Socket.IO 事件
+// });
 
 server.listen(3000, () => {
     console.log(`Server is running on port ${3000}`);
