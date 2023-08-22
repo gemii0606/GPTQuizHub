889142f17444bf33c88ef543ebe8d129be0a8eb6
gemii0606 <gemii0606@gmail.com>
2023-08-22 17:31:40 +0800

    Update app.js

--- back_end/app.js
+++ back_end/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const http = require('http'); // 引入 http 模块
+const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors'); // 引入 cors 模块
 
 const app = express();
-const server = http.createServer(app); // 使用 http 模块创建服务器
-const io = socketIo(server); // 将 Socket.IO 集成到服务器
+app.use(cors()); // 使用 cors 中间件
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// 其他 Express 中间件和路由设置
+io.on('connection', (socket) => {
+    console.log('A user connected');
 
-// io.on('connection', (socket) => {
-//     console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 
-//     socket.on('disconnect', () => {
-//         console.log('User disconnected');
-//     });
-    
-//     // 在此处理其他 Socket.IO 事件
-// });
+    // 在此处理其他 Socket.IO 事件
+});
 
 server.listen(3000, () => {
     console.log(`Server is running on port ${3000}`);
