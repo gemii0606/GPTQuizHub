50f786d4f9987334a41184f2a629b8a9fe430bcf
OAO1430 <trinky.cw.su@gmail.com>
2023-08-23 17:08:39 +0800

    merge

--- back_end/app.js
+++ back_end/app.js
@@ -2,15 +2,10 @@ const express = require('express');
 const routes = require('./server/routes');
 const app = express();
 var cors = require('cors');
+const socketIO = require('socket.io');
 const http = require('http');
 // const socketIo = require('socket.io');
 var cors = require('cors'); // 引入 cors 模块
-
-const { MongoClient, ObjectId } = require('mongodb');
-require('dotenv').config({ path: __dirname + `/.env` });
-
-const url = process.env.MONGOURL;
-const dbName = 'GPTQuizHub';
 
 
 app.use(cors()); // 使用 cors 中间件
@@ -28,7 +23,7 @@ io.on('connection', (socket) => {
     console.log('A user connected');
 
     socket.on('createroom', (user_id) => {
-        socket.join(socket.id);
+        socket.join(`room:${socket.id}`);
         console.log(`User ${user_id} created and joined room ${socket.id}`);
         roomConnections[socket.id] = {
             creater_id: user_id
@@ -50,43 +45,14 @@ io.on('connection', (socket) => {
 
     socket.on('isready', (roomName, user_id) => {
         console.log(`User ${user_id} is ready in room ${roomName}`);
-        console.log(roomName)
-        console.log(user_id)
         if (roomConnections[roomName].creater_id === user_id) {
-            roomConnections[roomName].creater_status = 'ok';
-            console.log(roomConnections)
+            roomConnections[roomName].creater_status = 'ok'
         }
         if (roomConnections[roomName].opponent_id === user_id) {
-            roomConnections[roomName].opponent_status = 'ok';
-            console.log(roomConnections)
+            roomConnections[roomName].opponent_status = 'ok'
         }
         if (roomConnections[roomName].creater_status === 'ok' && roomConnections[roomName].opponent_status === 'ok') {
-            console.log('in double status')
-            console.log(roomName)
-            console.log(user_id)
-            // const creater_id = new ObjectId(roomConnections[roomName].creater_id);
-            // const opponent_id = new ObjectId(roomConnections[roomName].opponent_id);
-
-            // const client = new MongoClient(url, { useUnifiedTopology: true });
-            // await client.connect();
-            // const db = client.db(dbName);
-            // const quizzesCollection = db.collection('quizzes');
-            // const allQuiz = await quizzesCollection.aggregate([
-            //     { $match: {
-            //         $or: [
-            //             { user_id: creater_id },
-            //             { user_id: opponent_id }
-            //         ]
-            //     }},
-            //     { $sample: { size: 1 } } // 随机选择一个文档
-            // ]).toArray();
-
-            // const randomQuizId = allQuiz[0]._id;
-            // console.log(randomQuizId)
-
-
             const data = {
-                "article": "test",
                 "quiz":{
                     "id": 123,
                     "title": "title",
@@ -148,15 +114,30 @@ io.on('connection', (socket) => {
                     ]
             }
         }
-        io.to(roomName).emit('isready', {data});
+        io.to(roomName).emit('start', {data});
         }
-
-        socket.on('disconnect', () => {
-            console.log(`User ${socket.id} disconnected`);
-            delete roomConnections[socket.id];
-        });
     });
+
+    socket.on('end', (roomName, user_id, users_score) => {
+        socket.join(`room:${socket.id}`);
+        if (!roomConnections[roomName].score)
+            roomConnections[roomName].score = [];
+        
+        roomConnections[roomName].score.push({user_id, score:user_score})
+        
+        
+        if (roomConnections[roomName].score.length == 2){
+            roomConnections[roomName].score.sort((a, b) => b.score - a.score);
+            const winner = roomConnections[roomName].score[0].user_id
+            socket.emit('end', roomConnections[roomName].score , winner)
+        }
+    })
 })
+/*
+app.listen(3000, () => {
+    console.log(`Ready. Listening in ${3000}`);
+}); 
+*/
 
 server.listen(3000, () => {
     console.log(`Server is running on port ${3000}`);
