f1869345a4c69f5293677e4b16203d51cbaedf10
Jarenchi <jaren719@gmail.com>
2023-08-24 10:36:07 +0800

    fix: fix quizsetting bug

--- frontend/components/QuizSetting.jsx
+++ frontend/components/QuizSetting.jsx
@@ -13,8 +13,6 @@ function QuizSetting({
   setQuestionSeconds,
   correctRatio,
   setCorrectRatio,
-  setUseCorrectRatio,
-  useCorrectRatio,
 }) {
   const questionSecondRef = useRef(null);
   const correctRatioRef = useRef(null);
@@ -33,43 +31,30 @@ function QuizSetting({
           {(pathname.includes("/twoplayer") || pathname.includes("/multiplayer")) && (
             <div className="flex items-center mt-5">
               <p className="mr-5 text-3xl">得分倍率</p>
+              <input
+                type="number"
+                min="1.01"
+                max="2"
+                step="0.01"
+                defaultValue={correctRatio}
+                ref={correctRatioRef}
+                className="px-2 mr-5 text-3xl border rounded-xl"
+              />
               <button
-                type="button"
-                className="px-[2.125rem] py-4 mr-5 text-white font-outfit font-normal text-xl leading-6 rounded-md bg-primary"
                 onClick={() => {
-                  setUseCorrectRatio((prev) => !prev);
+                  setCorrectRatio(correctRatioRef.current.value);
+                  Swal.fire({
+                    icon: "success",
+                    title: "得分倍率修改成功",
+                    showConfirmButton: false,
+                    timer: 1000,
+                  });
                 }}
+                type="button"
+                className="px-[2.125rem] py-4 text-white font-outfit font-normal text-3xl leading-6 rounded-md bg-sky-500"
               >
-                {useCorrectRatio ? "啟用" : "未啟用"}
+                更改得分倍率
               </button>
-              {useCorrectRatio && (
-                <>
-                  <input
-                    type="number"
-                    min="1.01"
-                    max="2"
-                    step="0.01"
-                    defaultValue={correctRatio}
-                    ref={correctRatioRef}
-                    className="px-2 mr-5 text-3xl border rounded-xl"
-                  />
-                  <button
-                    onClick={() => {
-                      setCorrectRatio(correctRatioRef.current.value);
-                      Swal.fire({
-                        icon: "success",
-                        title: "得分倍率修改成功",
-                        showConfirmButton: false,
-                        timer: 1000,
-                      });
-                    }}
-                    type="button"
-                    className="px-[2.125rem] py-4 text-white font-outfit font-normal text-3xl leading-6 rounded-md bg-sky-500"
-                  >
-                    更改得分倍率
-                  </button>
-                </>
-              )}
             </div>
           )}
           <div className="flex items-center mt-5">
